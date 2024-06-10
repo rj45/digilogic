2a5a1f0ff9974d7a06ba6fa014c893f2c36a089a
rj45 <ryan@rj45.ca>
2024-06-10 08:01:22 -0700

    Simplify change log and adapt for new ECS system

--- src/core/changelog.c
+++ src/core/changelog.c
@@ -14,23 +14,17 @@
    limitations under the License.
 */
 
-#include "core.h"
-#include <stdint.h>
+#include "core/core.h"
 
-#define MAX_FIELD_SIZE 8
-#define MAX_OBJECT_SIZE 40
+#define MAX_COMPONENT_SIZE 8
 
 typedef struct LogUpdate {
-  uint32_t fieldOffset;
-  uint32_t fieldSize;
-  uint8_t newValue[MAX_FIELD_SIZE];
-  uint8_t oldValue[MAX_FIELD_SIZE];
+  uint16_t table;
+  uint8_t column;
+  uint8_t size;
+  uint32_t row;
+  uint8_t newValue[MAX_COMPONENT_SIZE];
 } LogUpdate;
-
-typedef struct LogObject {
-  uint32_t size;
-  uint8_t data[MAX_OBJECT_SIZE];
-} LogObject;
 
 typedef struct LogEntry {
   enum {
@@ -45,21 +39,30 @@ typedef struct LogEntry {
 typedef struct ChangeLog {
   arr(LogEntry) log;
   arr(LogUpdate) updates;
-  arr(LogObject) objects;
   arr(size_t) commitPoints;
+  size_t redoIndex;
 
-  uint32_t redoIndex;
-
-  void (*cl_create_object)(ID id, void *object, size_t size);
-  void (*cl_delete_object)(ID id, void *object, size_t size);
-  void (*cl_update_object)(ID id, size_t fieldOffset, void *data, size_t size);
+  void *user;
+  void (*cl_revert_snapshot)(void *user);
+  void (*cl_replay_create)(void *user, ID id);
+  void (*cl_replay_delete)(void *user, ID id);
+  void (*cl_replay_update)(
+    void *user, ID id, uint16_t table, uint16_t column, uint32_t row,
+    void *data, size_t size);
 } ChangeLog;
 
-#define cl_record_update(log, id, ptr, field, value)                           \
-  (cl_record_update_(                                                          \
-     (log), (id), ((char *)(&((ptr)->(field))) - (char *)(ptr)),               \
-     sizeof((ptr)->(field)), &(value), &((ptr)->(field))),                     \
-   (ptr)->(field) = value)
+ChangeLog *cl_new() {
+  ChangeLog *log = malloc(sizeof(ChangeLog));
+  *log = (ChangeLog){0};
+  return log;
+}
+
+void cl_free(ChangeLog *log) {
+  arrfree(log->log);
+  arrfree(log->updates);
+  arrfree(log->commitPoints);
+  free(log);
+}
 
 void cl_commit(ChangeLog *log) {
   assert(log->redoIndex == arrlen(log->commitPoints));
@@ -67,78 +70,97 @@ void cl_commit(ChangeLog *log) {
   log->redoIndex++;
 }
 
-void cl_truncate_redo(ChangeLog *log) {
-  if (log->redoIndex < arrlen(log->commitPoints)) {
-    arrsetlen(log->commitPoints, log->redoIndex);
+static void cl_truncate_redo(ChangeLog *log) {
+  if (log->redoIndex >= arrlen(log->commitPoints)) {
+    return;
   }
+
+  ptrdiff_t commitPoint = (ptrdiff_t)log->commitPoints[log->redoIndex];
+  for (ptrdiff_t i = arrlen(log->log) - 1; i >= commitPoint; i++) {
+    if (log->log[i].verb == LOG_UPDATE) {
+      arrpop(log->updates);
+      assert(arrlen(log->updates) == log->log[i].dataIndex);
+    }
+  }
+  arrsetlen(log->log, commitPoint);
+  arrsetlen(log->commitPoints, log->redoIndex);
 }
 
-void cl_create(ChangeLog *log, ID id, void *object, size_t size) {
+void cl_create(ChangeLog *log, ID id) {
   cl_truncate_redo(log);
 
-  LogEntry entry = (LogEntry){LOG_CREATE, id, arrlen(log->objects)};
+  LogEntry entry = (LogEntry){LOG_CREATE, id};
   arrput(log->log, entry);
-
-  LogObject obj = (LogObject){size};
-  arrput(log->objects, obj);
-  memcpy(log->objects[arrlen(log->objects) - 1].data, object, size);
 }
 
-void cl_delete(ChangeLog *log, ID id, void *object, size_t size) {
+void cl_delete(ChangeLog *log, ID id) {
   cl_truncate_redo(log);
 
-  LogEntry entry = (LogEntry){LOG_DELETE, id, arrlen(log->objects)};
+  LogEntry entry = (LogEntry){LOG_DELETE, id};
   arrput(log->log, entry);
-
-  LogObject obj = (LogObject){size};
-  arrput(log->objects, obj);
-  memcpy(log->objects[arrlen(log->objects) - 1].data, object, size);
 }
 
-void cl_record_update_(
-  ChangeLog *log, ID id, size_t fieldOffset, size_t size, void *oldValue,
-  void *newValue) {
+void cl_update(
+  ChangeLog *log, ID id, uint16_t table, uint16_t column, uint32_t row,
+  void *newValue, size_t size) {
   cl_truncate_redo(log);
+  assert(size <= MAX_COMPONENT_SIZE);
+
+  // prevent bad things if asserts are turned off
+  size = size > MAX_COMPONENT_SIZE ? MAX_COMPONENT_SIZE : size;
 
   LogEntry entry = (LogEntry){LOG_UPDATE, id, arrlen(log->updates)};
   arrput(log->log, entry);
-  memcpy(log->updates[arrlen(log->updates)].oldValue, oldValue, size);
-  memcpy(log->updates[arrlen(log->updates)].newValue, newValue, size);
+  LogUpdate update = (LogUpdate){table, column, (uint8_t)size, row, {0}};
+  memcpy(update.newValue, newValue, size);
+  arrput(log->updates, update);
 }
 
-// undo all actions to the previous commit point
+static void cl_redo_range(ChangeLog *log, size_t start, size_t end) {
+  for (size_t i = start; i < end; i++) {
+    LogEntry *entry = &log->log[i];
+    switch (entry->verb) {
+    case LOG_CREATE:
+      log->cl_replay_create(log->user, entry->id);
+      break;
+    case LOG_DELETE:
+      log->cl_replay_delete(log->user, entry->id);
+      break;
+    case LOG_UPDATE: {
+      LogUpdate *update = &log->updates[entry->dataIndex];
+      log->cl_replay_update(
+        log->user, entry->id, update->table, update->column, update->row,
+        update->newValue, update->size);
+      break;
+    }
+    }
+  }
+}
+
+// Undo all actions to the previous commit point.
+// This is done by reverting to the snapshot and replaying all actions up to
+// the previous commit point.
 void cl_undo(ChangeLog *log) {
   if (log->redoIndex == 0) {
     return;
   }
 
   log->redoIndex--;
-  size_t commitIndex = log->commitPoints[log->redoIndex];
-  size_t goalIndex = 0;
-  if (log->redoIndex > 0) {
-    goalIndex = log->commitPoints[log->redoIndex - 1];
+  size_t commitPoint = log->commitPoints[log->redoIndex];
+
+  log->cl_revert_snapshot(log->user);
+
+  cl_redo_range(log, 0, commitPoint);
+}
+
+void cl_redo(ChangeLog *log) {
+  if ((log->redoIndex + 1) >= arrlen(log->commitPoints)) {
+    return;
   }
 
-  while (commitIndex >= goalIndex) {
-    LogEntry entry = log->log[commitIndex];
-    switch (entry.verb) {
-    case LOG_CREATE:
-      log->cl_delete_object(
-        entry.id, log->objects[entry.dataIndex].data,
-        log->objects[entry.dataIndex].size);
-      break;
-    case LOG_DELETE:
-      log->cl_create_object(
-        entry.id, log->objects[entry.dataIndex].data,
-        log->objects[entry.dataIndex].size);
-      break;
-    case LOG_UPDATE: {
-      LogUpdate *update = &log->updates[entry.dataIndex];
-      log->cl_update_object(
-        entry.id, update->fieldOffset, update->oldValue, update->fieldSize);
-      break;
-    }
-    }
-    commitIndex--;
-  }
+  size_t commitPoint = log->commitPoints[log->redoIndex];
+  size_t nextCommitPoint = log->commitPoints[log->redoIndex + 1];
+  cl_redo_range(log, commitPoint, nextCommitPoint);
+
+  log->redoIndex++;
 }
