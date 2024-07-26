c03e8e88250c4b95ef9a56341636878c800de491
Mathis Rech <mathisrech@gmx.de>
2024-07-26 17:36:17 +0200

    Implement small string optimization

--- crates/digilogic_core/src/lib.rs
+++ crates/digilogic_core/src/lib.rs
@@ -3,15 +3,26 @@ pub mod components;
 pub mod events;
 mod init;
 
+#[macro_use]
+extern crate static_assertions;
+
 use std::borrow::Borrow;
 use std::fmt;
 use std::hash::{Hash, Hasher};
 use std::ops::Deref;
 use std::sync::Arc;
 
+const SHARED_STR_INLINE_CAP: usize =
+    (std::mem::size_of::<usize>() * 3) - (std::mem::size_of::<u8>() * 2);
+const_assert!(SHARED_STR_INLINE_CAP <= (u8::MAX as usize));
+
 enum SharedStrRepr {
     Static(&'static str),
     Arc(Arc<str>),
+    Small {
+        len: u8,
+        data: [u8; SHARED_STR_INLINE_CAP],
+    },
 }
 
 #[repr(transparent)]
@@ -25,6 +36,13 @@ impl Deref for SharedStr {
         match self.0 {
             SharedStrRepr::Static(s) => s,
             SharedStrRepr::Arc(ref s) => s,
+            SharedStrRepr::Small { len, ref data } => {
+                #[allow(unsafe_code)]
+                unsafe {
+                    // SAFETY: `len` and `data` originate from a valid string slice
+                    std::str::from_utf8_unchecked(&data[..(len as usize)])
+                }
+            }
         }
     }
 }
@@ -48,6 +66,7 @@ impl Clone for SharedStr {
         match self.0 {
             SharedStrRepr::Static(s) => Self(SharedStrRepr::Static(s)),
             SharedStrRepr::Arc(ref s) => Self(SharedStrRepr::Arc(Arc::clone(s))),
+            SharedStrRepr::Small { len, data } => Self(SharedStrRepr::Small { len, data }),
         }
     }
 }
@@ -80,19 +99,55 @@ impl SharedStr {
     pub const fn new_static(s: &'static str) -> Self {
         Self(SharedStrRepr::Static(s))
     }
+
+    #[inline]
+    fn new_small(s: &str) -> Self {
+        assert!(s.len() <= SHARED_STR_INLINE_CAP);
+
+        let mut data = [0u8; SHARED_STR_INLINE_CAP];
+        data[..s.len()].copy_from_slice(s.as_bytes());
+
+        Self(SharedStrRepr::Small {
+            len: s.len() as u8,
+            data,
+        })
+    }
 }
 
 impl From<&str> for SharedStr {
-    #[inline]
     fn from(s: &str) -> Self {
-        Self(SharedStrRepr::Arc(s.into()))
+        if s.len() <= SHARED_STR_INLINE_CAP {
+            Self::new_small(s)
+        } else {
+            Self(SharedStrRepr::Arc(s.into()))
+        }
     }
 }
 
 impl From<String> for SharedStr {
-    #[inline]
     fn from(s: String) -> Self {
-        Self(SharedStrRepr::Arc(s.into()))
+        if s.len() <= SHARED_STR_INLINE_CAP {
+            Self::new_small(&s)
+        } else {
+            Self(SharedStrRepr::Arc(s.into()))
+        }
+    }
+}
+
+impl From<Box<str>> for SharedStr {
+    fn from(s: Box<str>) -> Self {
+        if s.len() <= SHARED_STR_INLINE_CAP {
+            Self::new_small(&s)
+        } else {
+            Self(SharedStrRepr::Arc(s.into()))
+        }
+    }
+}
+
+impl From<Arc<str>> for SharedStr {
+    #[inline]
+    fn from(s: Arc<str>) -> Self {
+        Self(SharedStrRepr::Arc(s))
     }
 }
 
