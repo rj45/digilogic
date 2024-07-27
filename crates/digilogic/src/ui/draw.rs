9963da88118a3fc0ed5f1cbf908e13aeeaf38626
rj45 <ryan@rj45.ca>
2024-07-27 09:24:07 -0700

    Switch from loading SVGs to using bez_path! macros

--- crates/digilogic/src/ui/draw.rs
+++ crates/digilogic/src/ui/draw.rs
@@ -1,6 +1,9 @@
 use bevy_ecs::prelude::*;
 use digilogic_core::components::Shape;
-use vello::kurbo::Affine;
+use vello::{
+    kurbo::{Affine, BezPath, Stroke},
+    peniko::{Brush, Color, Fill},
+};
 
 include!("bez_path.rs");
 
@@ -8,99 +11,134 @@ include!("bez_path.rs");
 pub struct Scene(pub vello::Scene);
 
 #[derive(Default, Resource)]
-pub struct SymbolSVGs(pub Vec<vello::Scene>);
+pub struct SymbolSVGs(pub Vec<BezPath>);
 
 pub fn draw(mut scene: ResMut<Scene>, symbol_svgs: Res<SymbolSVGs>) {
     let scene = &mut scene.0;
     scene.reset();
 
-    scene.append(
+    scene.fill(
+        Fill::NonZero,
+        Affine::scale(10.0).then_translate((100.0, 100.0).into()),
+        &Brush::Solid(Color::rgb8(128, 128, 128)),
+        None,
         &symbol_svgs.0[Shape::And as usize],
-        Some(Affine::scale(2.0).then_translate((100.0, 100.0).into())),
     );
 
-    scene.append(
+    scene.stroke(
+        &Stroke::new(0.25),
+        Affine::scale(10.0).then_translate((100.0, 100.0).into()),
+        &Brush::Solid(Color::rgb8(255, 255, 255)),
+        None,
+        &symbol_svgs.0[Shape::And as usize],
+    );
+
+    scene.fill(
+        Fill::NonZero,
+        Affine::scale(10.0).then_translate((100.0, 200.0).into()),
+        &Brush::Solid(Color::rgb8(128, 128, 128)),
+        None,
         &symbol_svgs.0[Shape::Or as usize],
-        Some(Affine::scale(2.0).then_translate((100.0, 200.0).into())),
     );
 
-    scene.append(
+    scene.stroke(
+        &Stroke::new(0.25),
+        Affine::scale(10.0).then_translate((100.0, 200.0).into()),
+        &Brush::Solid(Color::rgb8(255, 255, 255)),
+        None,
+        &symbol_svgs.0[Shape::Or as usize],
+    );
+
+    scene.fill(
+        Fill::NonZero,
+        Affine::scale(10.0).then_translate((100.0, 300.0).into()),
+        &Brush::Solid(Color::rgb8(128, 128, 128)),
+        None,
         &symbol_svgs.0[Shape::Xor as usize],
-        Some(Affine::scale(2.0).then_translate((100.0, 300.0).into())),
     );
 
-    scene.append(
+    scene.stroke(
+        &Stroke::new(0.25),
+        Affine::scale(10.0).then_translate((100.0, 300.0).into()),
+        &Brush::Solid(Color::rgb8(255, 255, 255)),
+        None,
+        &symbol_svgs.0[Shape::Xor as usize],
+    );
+
+    scene.fill(
+        Fill::NonZero,
+        Affine::scale(10.0).then_translate((100.0, 400.0).into()),
+        &Brush::Solid(Color::rgb8(128, 128, 128)),
+        None,
         &symbol_svgs.0[Shape::Not as usize],
-        Some(Affine::scale(2.0).then_translate((100.0, 400.0).into())),
     );
 
-    scene.append(
+    scene.stroke(
+        &Stroke::new(0.25),
+        Affine::scale(10.0).then_translate((100.0, 400.0).into()),
+        &Brush::Solid(Color::rgb8(255, 255, 255)),
+        None,
+        &symbol_svgs.0[Shape::Not as usize],
+    );
+
+    scene.fill(
+        Fill::NonZero,
+        Affine::scale(10.0).then_translate((100.0, 500.0).into()),
+        &Brush::Solid(Color::rgb8(128, 128, 128)),
+        None,
         &symbol_svgs.0[Shape::Input as usize],
-        Some(Affine::scale(2.0).then_translate((100.0, 500.0).into())),
     );
 
-    scene.append(
+    scene.stroke(
+        &Stroke::new(0.25),
+        Affine::scale(10.0).then_translate((100.0, 500.0).into()),
+        &Brush::Solid(Color::rgb8(255, 255, 255)),
+        None,
+        &symbol_svgs.0[Shape::Input as usize],
+    );
+
+    scene.fill(
+        Fill::NonZero,
+        Affine::scale(10.0).then_translate((100.0, 600.0).into()),
+        &Brush::Solid(Color::rgb8(128, 128, 128)),
+        None,
         &symbol_svgs.0[Shape::Output as usize],
-        Some(Affine::scale(2.0).then_translate((100.0, 600.0).into())),
+    );
+
+    scene.stroke(
+        &Stroke::new(0.25),
+        Affine::scale(10.0).then_translate((100.0, 600.0).into()),
+        &Brush::Solid(Color::rgb8(255, 255, 255)),
+        None,
+        &symbol_svgs.0[Shape::Output as usize],
     );
 }
 
 pub fn init_symbol_shapes(mut symbol_svgs: ResMut<SymbolSVGs>) {
-    use vello_svg::usvg::{Options, Size, Tree};
-
-    // Chip
-    let svg = Tree::from_str("<?xml version=\"1.0\" encoding=\"UTF-8\"?><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"1\" height=\"1\"/>", &Options::default()).unwrap();
-    symbol_svgs.0.push(vello_svg::render_tree(&svg));
-
-    // Port
-    let svg = Tree::from_str("<?xml version=\"1.0\" encoding=\"UTF-8\"?><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"3\" height=\"3\"><circle cx=\"1.5\" cy=\"1.5\" r=\"1.5\" /></svg>", &Options::default()).unwrap();
-    symbol_svgs.0.push(vello_svg::render_tree(&svg));
-
-    // And
-    let svg = Tree::from_data(
-        include_bytes!("../../assets/schemalib/symbols/schemalib-and2-l.svg"),
-        &Default::default(),
-    )
-    .unwrap();
-    symbol_svgs.0.push(vello_svg::render_tree(&svg));
-
-    // Or
-    let svg = Tree::from_data(
-        include_bytes!("../../assets/schemalib/symbols/schemalib-or2-l.svg"),
-        &Default::default(),
-    )
-    .unwrap();
-    symbol_svgs.0.push(vello_svg::render_tree(&svg));
-
-    // Xor
-    let svg = Tree::from_data(
-        include_bytes!("../../assets/schemalib/symbols/schemalib-xor2-l.svg"),
-        &Default::default(),
-    )
-    .unwrap();
-    symbol_svgs.0.push(vello_svg::render_tree(&svg));
-
-    // Not
-    let svg = Tree::from_data(
-        include_bytes!("../../assets/schemalib/symbols/schemalib-inv-l.svg"),
-        &Default::default(),
-    )
-    .unwrap();
-    symbol_svgs.0.push(vello_svg::render_tree(&svg));
-
-    // Input
-    let svg = Tree::from_data(
-        include_bytes!("../../assets/schemalib/symbols/digilogic-input-l.svg"),
-        &Default::default(),
-    )
-    .unwrap();
-    symbol_svgs.0.push(vello_svg::render_tree(&svg));
-
-    // Output
-    let svg = Tree::from_data(
-        include_bytes!("../../assets/schemalib/symbols/digilogic-output-l.svg"),
-        &Default::default(),
-    )
-    .unwrap();
-    symbol_svgs.0.push(vello_svg::render_tree(&svg));
+    symbol_svgs.0 = vec![
+        // Chip
+        bez_path!(),
+        // Port
+        bez_path!(),
+        // And -- from schemalib-and2-l.svg
+        bez_path!(M 5.9,7 H 3 V 1 L 5.9,1 C 7.7,1 9,2.2 9,4 9,5.8 7.4,7 5.9,7 Z),
+        // Or -- from schemalib-or2-l.svg
+        bez_path!(
+            M 3,7 H 4.4 C 6.7,7 7.7,6.9 9,4 7.7,1.1 6.7,1 4.4,1 H 3 C 4.4,3.1 4.4,4.9 3,7 Z
+        ),
+        // Xor -- from schemalib-xor2-l.svg
+        bez_path!(
+            M 3,7 H 4.4 C 6.7,7 7.7,6.9 9,4 7.7,1.1 6.7,1 4.4,1 H 3 C 4.4,3.1 4.4,4.9 3,7 Z
+            M 2.2,1 C 3.6,3.1 3.6,4.9 2.2,7
+        ),
+        // Not -- from schemalib-inv-l.svg
+        bez_path!(
+            M 7,3.7 C 6.6,3.7 6.3,3.4 6.3,3 6.3,2.6 6.6,2.3 7,2.3 7.4,2.3 7.7,2.6 7.7,3 7.7,3.4 7.4,3.7 7,3.7 Z
+            M 6.3,3 3.3,1.5 V 4.5 L 6.3,3 Z
+        ),
+        // Input
+        bez_path!(M 14,1 H 1 V 13 H 14 L 18,7 Z),
+        // Output
+        bez_path!(M 10,1 H 23 V 13 H 10 L 6,7 Z),
+    ];
 }
