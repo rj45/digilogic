c894b9f67f75447dbccf9b09ce7769fcf835a93a
Mathis Rech <mathisrech@gmx.de>
2024-08-01 00:52:57 +0200

    Use bitflags for PathKind

--- crates/digilogic/src/ui/draw.rs
+++ crates/digilogic/src/ui/draw.rs
@@ -1,4 +1,5 @@
 use bevy_ecs::prelude::*;
+use bitflags::bitflags;
 use digilogic_core::{components::Shape, transform::GlobalTransform};
 use vello::{
     kurbo::{Affine, BezPath, Stroke, Vec2},
@@ -10,10 +11,11 @@ include!("bez_path.rs");
 #[derive(Default, Resource)]
 pub struct Scene(pub vello::Scene);
 
-pub enum PathKind {
-    Fill,
-    Stroke,
-    FillStroke,
+bitflags! {
+    pub struct PathKind: u8 {
+        const FILL = 0x1;
+        const STROKE = 0x2;
+    }
 }
 
 struct PathInfo {
@@ -24,11 +26,13 @@ struct PathInfo {
 #[derive(Default)]
 pub struct SymbolShape {
     paths: Vec<PathInfo>,
-    scale: f32,
+    scale: f64,
 }
 
 #[derive(Default, Resource)]
 pub struct SymbolShapes(pub Vec<SymbolShape>);
+
+const SYMBOL_STROKE_WIDTH: f64 = 5.0;
 
 pub fn draw(
     mut scene: ResMut<Scene>,
@@ -39,46 +43,33 @@ pub fn draw(
     scene.reset();
 
     for (&shape, transform) in shapes.iter() {
-        let symbolShape = &symbol_svgs.0[shape as usize];
-        let transform = Affine::scale(symbolShape.scale as f64)
+        let symbol_shape = &symbol_svgs.0[shape as usize];
+        let transform = Affine::scale(symbol_shape.scale)
             .then_rotate(transform.rotation.radians())
             .then_translate(Vec2::new(
                 transform.translation.x as f64,
                 transform.translation.y as f64,
             ));
 
-        for path in symbolShape.paths.iter() {
-            match path.kind {
-                PathKind::Fill => scene.fill(
+        for path in symbol_shape.paths.iter() {
+            if path.kind.contains(PathKind::FILL) {
+                scene.fill(
                     Fill::NonZero,
                     transform,
                     &Brush::Solid(Color::GRAY),
                     None,
                     &path.path,
-                ),
-                PathKind::Stroke => scene.stroke(
-                    &Stroke::new(0.5),
+                );
+            }
+
+            if path.kind.contains(PathKind::STROKE) {
+                scene.stroke(
+                    &Stroke::new(SYMBOL_STROKE_WIDTH / symbol_shape.scale),
                     transform,
                     &Brush::Solid(Color::WHITE),
                     None,
                     &path.path,
-                ),
-                PathKind::FillStroke => {
-                    scene.fill(
-                        Fill::NonZero,
-                        transform,
-                        &Brush::Solid(Color::GRAY),
-                        None,
-                        &path.path,
-                    );
-                    scene.stroke(
-                        &Stroke::new(0.5),
-                        transform,
-                        &Brush::Solid(Color::WHITE),
-                        None,
-                        &path.path,
-                    );
-                }
+                );
             }
         }
     }
@@ -89,7 +80,7 @@ pub fn init_symbol_shapes(mut symbol_svgs: ResMut<SymbolShapes>) {
         // Chip
         SymbolShape {
             paths: vec![PathInfo {
-                kind: PathKind::Fill,
+                kind: PathKind::FILL,
                 path: bez_path!(),
             }],
             scale: 10.0,
@@ -97,7 +88,7 @@ pub fn init_symbol_shapes(mut symbol_svgs: ResMut<SymbolShapes>) {
         // Port
         SymbolShape {
             paths: vec![PathInfo {
-                kind: PathKind::Fill,
+                kind: PathKind::FILL,
                 path: bez_path!(),
             }],
             scale: 10.0,
@@ -105,7 +96,7 @@ pub fn init_symbol_shapes(mut symbol_svgs: ResMut<SymbolShapes>) {
         // And -- from schemalib-and2-l.svg
         SymbolShape {
             paths: vec![PathInfo {
-                kind: PathKind::FillStroke,
+                kind: PathKind::FILL | PathKind::STROKE,
                 path: bez_path!(M 5.9,7 H 3 V 1 L 5.9,1 C 7.7,1 9,2.2 9,4 9,5.8 7.4,7 5.9,7 Z),
             }],
             scale: 10.0,
@@ -113,7 +104,7 @@ pub fn init_symbol_shapes(mut symbol_svgs: ResMut<SymbolShapes>) {
         // Or -- from schemalib-or2-l.svg
         SymbolShape {
             paths: vec![PathInfo {
-                kind: PathKind::FillStroke,
+                kind: PathKind::FILL | PathKind::STROKE,
                 path: bez_path!(
                     M 3,7 H 4.4 C 6.7,7 7.7,6.9 9,4 7.7,1.1 6.7,1 4.4,1 H 3 C 4.4,3.1 4.4,4.9 3,7 Z
                 ),
@@ -124,13 +115,13 @@ pub fn init_symbol_shapes(mut symbol_svgs: ResMut<SymbolShapes>) {
         SymbolShape {
             paths: vec![
                 PathInfo {
-                    kind: PathKind::FillStroke,
+                    kind: PathKind::FILL | PathKind::STROKE,
                     path: bez_path!(
                         M 3,7 H 4.4 C 6.7,7 7.7,6.9 9,4 7.7,1.1 6.7,1 4.4,1 H 3 C 4.4,3.1 4.4,4.9 3,7 Z
                     ),
                 },
                 PathInfo {
-                    kind: PathKind::Stroke,
+                    kind: PathKind::STROKE,
                     path: bez_path!(
                         M 2.2,1 C 3.6,3.1 3.6,4.9 2.2,7
                     ),
@@ -142,13 +133,13 @@ pub fn init_symbol_shapes(mut symbol_svgs: ResMut<SymbolShapes>) {
         SymbolShape {
             paths: vec![
                 PathInfo {
-                    kind: PathKind::FillStroke,
+                    kind: PathKind::FILL | PathKind::STROKE,
                     path: bez_path!(
                         M 7,3.7 C 6.6,3.7 6.3,3.4 6.3,3 6.3,2.6 6.6,2.3 7,2.3 7.4,2.3 7.7,2.6 7.7,3 7.7,3.4 7.4,3.7 7,3.7 Z
                     ),
                 },
                 PathInfo {
-                    kind: PathKind::FillStroke,
+                    kind: PathKind::FILL | PathKind::STROKE,
                     path: bez_path!(
                         M 6.3,3 3.3,1.5 V 4.5 L 6.3,3 Z
                     ),
@@ -159,7 +150,7 @@ pub fn init_symbol_shapes(mut symbol_svgs: ResMut<SymbolShapes>) {
         // Input
         SymbolShape {
             paths: vec![PathInfo {
-                kind: PathKind::FillStroke,
+                kind: PathKind::FILL | PathKind::STROKE,
                 path: bez_path!(M 14,1 H 1 V 13 H 14 L 18,7 Z),
             }],
             scale: 4.0,
@@ -167,7 +158,7 @@ pub fn init_symbol_shapes(mut symbol_svgs: ResMut<SymbolShapes>) {
         // Output
         SymbolShape {
             paths: vec![PathInfo {
-                kind: PathKind::FillStroke,
+                kind: PathKind::FILL | PathKind::STROKE,
                 path: bez_path!(M 10,1 H 23 V 13 H 10 L 6,7 Z),
             }],
             scale: 4.0,
