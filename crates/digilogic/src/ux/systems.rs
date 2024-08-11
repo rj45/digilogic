2fe8c7b2124cb257e528658113020fce4a863917
Mathis Rech <mathisrech@gmx.de>
2024-08-11 23:03:30 +0200

    Disallow ports from being moved

--- crates/digilogic/src/ux/systems.rs
+++ crates/digilogic/src/ux/systems.rs
@@ -6,7 +6,7 @@ use bevy_ecs::prelude::*;
 use digilogic_core::components::*;
 use digilogic_core::spatial_index::SpatialIndex;
 use digilogic_core::transform::{BoundingBox, Transform, Vec2};
-use digilogic_core::{fixed, Fixed};
+use digilogic_core::Fixed;
 
 /// Called when a new viewport is added to the world.
 pub(crate) fn on_add_viewport_augment_with_fsm(
@@ -26,7 +26,7 @@ fn mouse_state_transition_system(
     trigger: Trigger<PointerButtonEvent>,
     hover_query: Query<&HoveredEntity>,
     mut mouse_state_query: Query<&mut MouseState>,
-    transform_query: Query<&Transform>,
+    transform_query: Query<(&Transform, Has<Port>)>,
     mut commands: Commands,
 ) {
     let viewport = trigger.entity();
@@ -42,16 +42,22 @@ fn mouse_state_transition_system(
                 };
 
                 let mut offset_list = Vec::new();
-                if let Ok(transform) = transform_query.get(hovered_entity) {
-                    offset_list.push(EntityOffset {
-                        entity: hovered_entity,
-                        offset: transform.translation - mouse_pos,
-                    });
+                if let Ok((transform, is_port)) = transform_query.get(hovered_entity) {
+                    if is_port {
+                        // TODO: enter wire drawing mode
+                    } else {
+                        offset_list.push(EntityOffset {
+                            entity: hovered_entity,
+                            offset: transform.translation - mouse_pos,
+                        });
+                    }
                 }
 
-                *mouse_state = MouseState::Moving(offset_list.clone());
-                commands.entity(viewport).remove::<MouseIdle>();
-                commands.entity(viewport).insert(MouseMoving(offset_list));
+                if !offset_list.is_empty() {
+                    *mouse_state = MouseState::Moving(offset_list.clone());
+                    commands.entity(viewport).remove::<MouseIdle>();
+                    commands.entity(viewport).insert(MouseMoving(offset_list));
+                }
             }
         }
         (MouseState::Moving(..), false) => {
@@ -62,8 +68,6 @@ fn mouse_state_transition_system(
         _ => {}
     }
 }
-
-const MOUSE_POS_FUDGE: Fixed = fixed!(1);
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(u8)]
@@ -89,8 +93,8 @@ fn hover_system(
             x: Fixed::try_from_f32(position.x).unwrap(),
             y: Fixed::try_from_f32(position.y).unwrap(),
         },
-        MOUSE_POS_FUDGE,
-        MOUSE_POS_FUDGE,
+        Fixed::EPSILON,
+        Fixed::EPSILON,
     );
 
     let mut new_hovered_entity = None;
