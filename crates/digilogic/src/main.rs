e019f2b4928700a320950615b749c1533bebafb1
Mathis Rech <mathisrech@gmx.de>
2024-08-16 20:52:37 +0200

    Setup networking crate

--- crates/digilogic/src/main.rs
+++ crates/digilogic/src/main.rs
@@ -63,6 +63,7 @@ impl App {
             bevy_core::TaskPoolPlugin::default(),
             bevy_core::TypeRegistrationPlugin,
             bevy_core::FrameCountPlugin,
+            bevy_time::TimePlugin,
             bevy_log::LogPlugin {
                 #[cfg(debug_assertions)]
                 level: bevy_log::Level::DEBUG,
@@ -89,6 +90,7 @@ impl App {
             digilogic_core::CorePlugin,
             digilogic_serde::LoadSavePlugin,
             digilogic_routing::RoutingPlugin,
+            digilogic_netcode::ClientPlugin,
             ux::UxPlugin::default(),
             ui::UiPlugin::new(context, render_state),
         ));
---
+++ crates/digilogic_netcode/src/lib.rs
@@ -0,0 +1,47 @@
+use renet::*;
+use digilogic_core::components::Circuit;
+use digilogic_core::HashMap;
+
+#[derive(Default)]
+#[cfg_attr(feature = "client", derive(bevy_ecs::prelude::Component))]
+#[allow(missing_debug_implementations)]
+pub struct SimState {
+    // TODO: these are just placeholders
+    nets: Vec<u32>,
+    components: HashMap<u32, u32>,
+}
+
+#[cfg(feature = "client")]
+mod client {
+    use super::*;
+    use bevy_app::prelude::*;
+    use bevy_ecs::prelude::*;
+    use bevy_time::prelude::*;
+
+    fn inject_sim_state(trigger: Trigger<OnAdd, Circuit>, mut commands: Commands) {
+        commands
+            .get_entity(trigger.entity())
+            .unwrap()
+            .insert(SimState::default());
+    }
+
+    fn update_client(mut client: ResMut<RenetClient>, time: Res<Time>) {
+        client.update(time.delta())
+    }
+
+    #[derive(Default, Debug)]
+    pub struct ClientPlugin;
+
+    impl Plugin for ClientPlugin {
+        fn build(&self, app: &mut App) {
+            app.observe(inject_sim_state);
+            app.add_systems(
+                PreUpdate,
+                update_client.run_if(resource_exists::<RenetClient>),
+            );
+        }
+    }
+}
+
+#[cfg(feature = "client")]
+pub use client::*;
