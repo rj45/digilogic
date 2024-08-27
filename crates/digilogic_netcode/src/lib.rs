ba01521f4831025be546384660a77d444600b2cd
Mathis Rech <mathisrech@gmx.de>
2024-08-27 03:32:54 +0200

    Populate Gsim sim state

--- crates/digilogic_netcode/src/lib.rs
+++ crates/digilogic_netcode/src/lib.rs
@@ -124,7 +124,7 @@ struct ClientMessage {
     kind: ClientMessageKind,
 }
 
-// Invariant for this struct: `state_bits` and `valid_bits` vectors need to contain at least one unused bit at any time
+// Invariant for this struct: bit planes need to contain at least one unused bit at any time
 #[derive(Serialize, Deserialize)]
 #[cfg_attr(feature = "client", derive(bevy_ecs::prelude::Component))]
 #[allow(missing_debug_implementations)]
@@ -132,9 +132,9 @@ pub struct SimState {
     order: u64, // To detect if message order gets messed up
     bit_len: u64,
     #[serde(with = "serde_bytes")]
-    state_bits: Vec<u8>,
+    bit_plane_0: Vec<u8>,
     #[serde(with = "serde_bytes")]
-    valid_bits: Vec<u8>,
+    bit_plane_1: Vec<u8>,
 }
 
 impl Default for SimState {
@@ -142,8 +142,8 @@ impl Default for SimState {
         Self {
             order: 0,
             bit_len: 0,
-            state_bits: vec![0],
-            valid_bits: vec![0],
+            bit_plane_0: vec![0],
+            bit_plane_1: vec![0],
         }
     }
 }
@@ -191,21 +191,26 @@ impl SimState {
     pub fn reset(&mut self) {
         self.order += 1;
         self.bit_len = 0;
-        self.state_bits.clear();
-        self.state_bits.push(0);
-        self.valid_bits.clear();
-        self.valid_bits.push(0);
+        self.bit_plane_0.clear();
+        self.bit_plane_0.push(0);
+        self.bit_plane_1.clear();
+        self.bit_plane_1.push(0);
     }
 
-    pub fn push_net(&mut self, bit_width: NonZeroU8, state_bits: &[u8], valid_bits: &[u8]) -> u64 {
+    pub fn push_net(
+        &mut self,
+        bit_width: NonZeroU8,
+        bit_plane_0: &[u8],
+        bit_plane_1: &[u8],
+    ) -> u64 {
         // Make sure invariant is upheld
-        debug_assert_eq!(self.state_bits.len(), self.valid_bits.len());
-        debug_assert!(((self.state_bits.len() as u64) * 8) > self.bit_len);
+        debug_assert_eq!(self.bit_plane_0.len(), self.bit_plane_1.len());
+        debug_assert!(((self.bit_plane_0.len() as u64) * 8) > self.bit_len);
 
         let bit_offset = (self.bit_len % 8) as u8;
         let byte_width = bit_width.get().div_ceil(8) as usize;
-        assert!(state_bits.len() >= byte_width);
-        assert!(valid_bits.len() >= byte_width);
+        assert!(bit_plane_0.len() >= byte_width);
+        assert!(bit_plane_1.len() >= byte_width);
 
         let mut remaining_bit_width = bit_width.get();
         for i in 0..byte_width {
@@ -213,13 +218,13 @@ impl SimState {
             remaining_bit_width -= byte_bit_width;
 
             let mask = mask(byte_bit_width);
-            let (state_low, state_high) = unalign_byte(state_bits[i] & mask, bit_offset);
-            let (valid_low, valid_high) = unalign_byte(valid_bits[i] & mask, bit_offset);
-            *self.state_bits.last_mut().unwrap() |= state_low;
-            *self.valid_bits.last_mut().unwrap() |= valid_low;
+            let (low_0, high_0) = unalign_byte(bit_plane_0[i] & mask, bit_offset);
+            let (low_1, high_1) = unalign_byte(bit_plane_1[i] & mask, bit_offset);
+            *self.bit_plane_0.last_mut().unwrap() |= low_0;
+            *self.bit_plane_1.last_mut().unwrap() |= low_1;
             if (bit_offset + byte_bit_width) >= 8 {
-                self.state_bits.push(state_high);
-                self.valid_bits.push(valid_high);
+                self.bit_plane_0.push(high_0);
+                self.bit_plane_1.push(high_1);
             }
         }
 
@@ -232,40 +237,40 @@ impl SimState {
         &self,
         offset: u64,
         bit_width: NonZeroU8,
-        state_bits: &mut [u8],
-        valid_bits: &mut [u8],
+        bit_plane_0: &mut [u8],
+        bit_plane_1: &mut [u8],
     ) {
         // Make sure invariant is upheld
-        debug_assert_eq!(self.state_bits.len(), self.valid_bits.len());
-        debug_assert!(((self.state_bits.len() as u64) * 8) > self.bit_len);
+        debug_assert_eq!(self.bit_plane_0.len(), self.bit_plane_1.len());
+        debug_assert!(((self.bit_plane_0.len() as u64) * 8) > self.bit_len);
 
         assert!((offset + (bit_width.get() as u64)) <= self.bit_len);
 
         let byte_offset = (offset / 8) as usize;
         let bit_offset = (offset % 8) as u8;
         let byte_width = bit_width.get().div_ceil(8) as usize;
-        assert!(state_bits.len() >= byte_width);
-        assert!(valid_bits.len() >= byte_width);
+        assert!(bit_plane_0.len() >= byte_width);
+        assert!(bit_plane_1.len() >= byte_width);
 
         let mut remaining_bit_width = bit_width.get();
         for i in 0..byte_width {
             let byte_bit_width = remaining_bit_width.min(8);
             remaining_bit_width -= byte_bit_width;
 
-            let state_low = self.state_bits[byte_offset + i];
-            let valid_low = self.valid_bits[byte_offset + i];
-            let (state_high, valid_high) = if (bit_offset + byte_bit_width) >= 8 {
+            let low_0 = self.bit_plane_0[byte_offset + i];
+            let low_1 = self.bit_plane_1[byte_offset + i];
+            let (high_0, high_1) = if (bit_offset + byte_bit_width) >= 8 {
                 (
-                    self.state_bits[byte_offset + i + 1],
-                    self.valid_bits[byte_offset + i + 1],
+                    self.bit_plane_0[byte_offset + i + 1],
+                    self.bit_plane_1[byte_offset + i + 1],
                 )
             } else {
                 (0, 0)
             };
 
             let mask = mask(byte_bit_width);
-            state_bits[i] = align_byte(state_low, state_high, bit_offset) & mask;
-            valid_bits[i] = align_byte(valid_low, valid_high, bit_offset) & mask;
+            bit_plane_0[i] = align_byte(low_0, high_0, bit_offset) & mask;
+            bit_plane_1[i] = align_byte(low_1, high_1, bit_offset) & mask;
         }
     }
 }
@@ -298,8 +303,8 @@ mod tests {
         let mut sim_state = SimState::default();
         let net_index = sim_state.push_net(nz!(1), &[0b1], &[0b1]);
         assert_eq!(net_index, 0);
-        assert_eq!(sim_state.state_bits, [0b1]);
-        assert_eq!(sim_state.valid_bits, [0b1]);
+        assert_eq!(sim_state.bit_plane_0, [0b1]);
+        assert_eq!(sim_state.bit_plane_1, [0b1]);
     }
 
     #[test]
@@ -307,14 +312,14 @@ mod tests {
         let sim_state = SimState {
             order: 0,
             bit_len: 1,
-            state_bits: vec![0b1],
-            valid_bits: vec![0b1],
+            bit_plane_0: vec![0b1],
+            bit_plane_1: vec![0b1],
         };
-        let mut state_bits = [0u8; 1];
-        let mut valid_bits = [0u8; 1];
-        sim_state.get_net(0, nz!(1), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0b1]);
-        assert_eq!(valid_bits, [0b1]);
+        let mut bit_plane_0 = [0u8; 1];
+        let mut bit_plane_1 = [0u8; 1];
+        sim_state.get_net(0, nz!(1), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0b1]);
+        assert_eq!(bit_plane_1, [0b1]);
     }
 
     #[test]
@@ -322,8 +327,8 @@ mod tests {
         let mut sim_state = SimState::default();
         let net_index = sim_state.push_net(nz!(8), &[0xAA], &[0x55]);
         assert_eq!(net_index, 0);
-        assert_eq!(sim_state.state_bits, [0xAA, 0]);
-        assert_eq!(sim_state.valid_bits, [0x55, 0]);
+        assert_eq!(sim_state.bit_plane_0, [0xAA, 0]);
+        assert_eq!(sim_state.bit_plane_1, [0x55, 0]);
     }
 
     #[test]
@@ -331,14 +336,14 @@ mod tests {
         let sim_state = SimState {
             order: 0,
             bit_len: 8,
-            state_bits: vec![0xAA, 0],
-            valid_bits: vec![0x55, 0],
+            bit_plane_0: vec![0xAA, 0],
+            bit_plane_1: vec![0x55, 0],
         };
-        let mut state_bits = [0u8; 1];
-        let mut valid_bits = [0u8; 1];
-        sim_state.get_net(0, nz!(8), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0xAA]);
-        assert_eq!(valid_bits, [0x55]);
+        let mut bit_plane_0 = [0u8; 1];
+        let mut bit_plane_1 = [0u8; 1];
+        sim_state.get_net(0, nz!(8), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0xAA]);
+        assert_eq!(bit_plane_1, [0x55]);
     }
 
     #[test]
@@ -346,8 +351,8 @@ mod tests {
         let mut sim_state = SimState::default();
         let net_index = sim_state.push_net(nz!(16), &[0xAA, 0x55], &[0x55, 0xAA]);
         assert_eq!(net_index, 0);
-        assert_eq!(sim_state.state_bits, [0xAA, 0x55, 0]);
-        assert_eq!(sim_state.valid_bits, [0x55, 0xAA, 0]);
+        assert_eq!(sim_state.bit_plane_0, [0xAA, 0x55, 0]);
+        assert_eq!(sim_state.bit_plane_1, [0x55, 0xAA, 0]);
     }
 
     #[test]
@@ -355,14 +360,14 @@ mod tests {
         let sim_state = SimState {
             order: 0,
             bit_len: 16,
-            state_bits: vec![0xAA, 0x55, 0],
-            valid_bits: vec![0x55, 0xAA, 0],
+            bit_plane_0: vec![0xAA, 0x55, 0],
+            bit_plane_1: vec![0x55, 0xAA, 0],
         };
-        let mut state_bits = [0u8; 2];
-        let mut valid_bits = [0u8; 2];
-        sim_state.get_net(0, nz!(16), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0xAA, 0x55]);
-        assert_eq!(valid_bits, [0x55, 0xAA]);
+        let mut bit_plane_0 = [0u8; 2];
+        let mut bit_plane_1 = [0u8; 2];
+        sim_state.get_net(0, nz!(16), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0xAA, 0x55]);
+        assert_eq!(bit_plane_1, [0x55, 0xAA]);
     }
 
     #[test]
@@ -370,8 +375,8 @@ mod tests {
         let mut sim_state = SimState::default();
         let net_index = sim_state.push_net(nz!(7), &[0x2A], &[0x55]);
         assert_eq!(net_index, 0);
-        assert_eq!(sim_state.state_bits, [0x2A]);
-        assert_eq!(sim_state.valid_bits, [0x55]);
+        assert_eq!(sim_state.bit_plane_0, [0x2A]);
+        assert_eq!(sim_state.bit_plane_1, [0x55]);
     }
 
     #[test]
@@ -379,14 +384,14 @@ mod tests {
         let sim_state = SimState {
             order: 0,
             bit_len: 7,
-            state_bits: vec![0x2A],
-            valid_bits: vec![0x55],
+            bit_plane_0: vec![0x2A],
+            bit_plane_1: vec![0x55],
         };
-        let mut state_bits = [0u8; 1];
-        let mut valid_bits = [0u8; 1];
-        sim_state.get_net(0, nz!(7), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0x2A]);
-        assert_eq!(valid_bits, [0x55]);
+        let mut bit_plane_0 = [0u8; 1];
+        let mut bit_plane_1 = [0u8; 1];
+        sim_state.get_net(0, nz!(7), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0x2A]);
+        assert_eq!(bit_plane_1, [0x55]);
     }
 
     #[test]
@@ -394,8 +399,8 @@ mod tests {
         let mut sim_state = SimState::default();
         let net_index = sim_state.push_net(nz!(9), &[0xAA, 0b0], &[0x55, 0b1]);
         assert_eq!(net_index, 0);
-        assert_eq!(sim_state.state_bits, [0xAA, 0b0]);
-        assert_eq!(sim_state.valid_bits, [0x55, 0b1]);
+        assert_eq!(sim_state.bit_plane_0, [0xAA, 0b0]);
+        assert_eq!(sim_state.bit_plane_1, [0x55, 0b1]);
     }
 
     #[test]
@@ -403,14 +408,14 @@ mod tests {
         let sim_state = SimState {
             order: 0,
             bit_len: 9,
-            state_bits: vec![0xAA, 0b0],
-            valid_bits: vec![0x55, 0b1],
+            bit_plane_0: vec![0xAA, 0b0],
+            bit_plane_1: vec![0x55, 0b1],
         };
-        let mut state_bits = [0u8; 2];
-        let mut valid_bits = [0u8; 2];
-        sim_state.get_net(0, nz!(9), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0xAA, 0b0]);
-        assert_eq!(valid_bits, [0x55, 0b1]);
+        let mut bit_plane_0 = [0u8; 2];
+        let mut bit_plane_1 = [0u8; 2];
+        sim_state.get_net(0, nz!(9), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0xAA, 0b0]);
+        assert_eq!(bit_plane_1, [0x55, 0b1]);
     }
 
     #[test]
@@ -422,8 +427,8 @@ mod tests {
         assert_eq!(net_index_a, 0);
         assert_eq!(net_index_b, 1);
         assert_eq!(net_index_c, 2);
-        assert_eq!(sim_state.state_bits, [0b101]);
-        assert_eq!(sim_state.valid_bits, [0b010]);
+        assert_eq!(sim_state.bit_plane_0, [0b101]);
+        assert_eq!(sim_state.bit_plane_1, [0b010]);
     }
 
     #[test]
@@ -431,20 +436,20 @@ mod tests {
         let sim_state = SimState {
             order: 0,
             bit_len: 3,
-            state_bits: vec![0b101],
-            valid_bits: vec![0b010],
+            bit_plane_0: vec![0b101],
+            bit_plane_1: vec![0b010],
         };
-        let mut state_bits = [0u8; 1];
-        let mut valid_bits = [0u8; 1];
-        sim_state.get_net(0, nz!(1), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0b1]);
-        assert_eq!(valid_bits, [0b0]);
-        sim_state.get_net(1, nz!(1), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0b0]);
-        assert_eq!(valid_bits, [0b1]);
-        sim_state.get_net(2, nz!(1), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0b1]);
-        assert_eq!(valid_bits, [0b0]);
+        let mut bit_plane_0 = [0u8; 1];
+        let mut bit_plane_1 = [0u8; 1];
+        sim_state.get_net(0, nz!(1), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0b1]);
+        assert_eq!(bit_plane_1, [0b0]);
+        sim_state.get_net(1, nz!(1), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0b0]);
+        assert_eq!(bit_plane_1, [0b1]);
+        sim_state.get_net(2, nz!(1), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0b1]);
+        assert_eq!(bit_plane_1, [0b0]);
     }
 
     #[test]
@@ -456,8 +461,8 @@ mod tests {
         assert_eq!(net_index_a, 0);
         assert_eq!(net_index_b, 8);
         assert_eq!(net_index_c, 16);
-        assert_eq!(sim_state.state_bits, [0xAA, 0x55, 0xAA, 0]);
-        assert_eq!(sim_state.valid_bits, [0x55, 0xAA, 0x55, 0]);
+        assert_eq!(sim_state.bit_plane_0, [0xAA, 0x55, 0xAA, 0]);
+        assert_eq!(sim_state.bit_plane_1, [0x55, 0xAA, 0x55, 0]);
     }
 
     #[test]
@@ -465,20 +470,20 @@ mod tests {
         let sim_state = SimState {
             order: 0,
             bit_len: 24,
-            state_bits: vec![0xAA, 0x55, 0xAA, 0],
-            valid_bits: vec![0x55, 0xAA, 0x55, 0],
+            bit_plane_0: vec![0xAA, 0x55, 0xAA, 0],
+            bit_plane_1: vec![0x55, 0xAA, 0x55, 0],
         };
-        let mut state_bits = [0u8; 1];
-        let mut valid_bits = [0u8; 1];
-        sim_state.get_net(0, nz!(8), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0xAA]);
-        assert_eq!(valid_bits, [0x55]);
-        sim_state.get_net(8, nz!(8), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0x55]);
-        assert_eq!(valid_bits, [0xAA]);
-        sim_state.get_net(16, nz!(8), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0xAA]);
-        assert_eq!(valid_bits, [0x55]);
+        let mut bit_plane_0 = [0u8; 1];
+        let mut bit_plane_1 = [0u8; 1];
+        sim_state.get_net(0, nz!(8), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0xAA]);
+        assert_eq!(bit_plane_1, [0x55]);
+        sim_state.get_net(8, nz!(8), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0x55]);
+        assert_eq!(bit_plane_1, [0xAA]);
+        sim_state.get_net(16, nz!(8), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0xAA]);
+        assert_eq!(bit_plane_1, [0x55]);
     }
 
     #[test]
@@ -490,8 +495,8 @@ mod tests {
         assert_eq!(net_index_a, 0);
         assert_eq!(net_index_b, 7);
         assert_eq!(net_index_c, 14);
-        assert_eq!(sim_state.state_bits, [0xAA, 0xAA, 0x0A]);
-        assert_eq!(sim_state.valid_bits, [0x55, 0x55, 0x15]);
+        assert_eq!(sim_state.bit_plane_0, [0xAA, 0xAA, 0x0A]);
+        assert_eq!(sim_state.bit_plane_1, [0x55, 0x55, 0x15]);
     }
 
     #[test]
@@ -499,20 +504,20 @@ mod tests {
         let sim_state = SimState {
             order: 0,
             bit_len: 21,
-            state_bits: vec![0xAA, 0xAA, 0x0A],
-            valid_bits: vec![0x55, 0x55, 0x15],
+            bit_plane_0: vec![0xAA, 0xAA, 0x0A],
+            bit_plane_1: vec![0x55, 0x55, 0x15],
         };
-        let mut state_bits = [0u8; 1];
-        let mut valid_bits = [0u8; 1];
-        sim_state.get_net(0, nz!(7), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0x2A]);
-        assert_eq!(valid_bits, [0x55]);
-        sim_state.get_net(7, nz!(7), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0x55]);
-        assert_eq!(valid_bits, [0x2A]);
-        sim_state.get_net(14, nz!(7), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0x2A]);
-        assert_eq!(valid_bits, [0x55]);
+        let mut bit_plane_0 = [0u8; 1];
+        let mut bit_plane_1 = [0u8; 1];
+        sim_state.get_net(0, nz!(7), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0x2A]);
+        assert_eq!(bit_plane_1, [0x55]);
+        sim_state.get_net(7, nz!(7), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0x55]);
+        assert_eq!(bit_plane_1, [0x2A]);
+        sim_state.get_net(14, nz!(7), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0x2A]);
+        assert_eq!(bit_plane_1, [0x55]);
     }
 
     #[test]
@@ -524,8 +529,8 @@ mod tests {
         assert_eq!(net_index_a, 0);
         assert_eq!(net_index_b, 9);
         assert_eq!(net_index_c, 18);
-        assert_eq!(sim_state.state_bits, [0xAA, 0xAA, 0xAA, 0x02]);
-        assert_eq!(sim_state.valid_bits, [0x55, 0x55, 0x55, 0x05]);
+        assert_eq!(sim_state.bit_plane_0, [0xAA, 0xAA, 0xAA, 0x02]);
+        assert_eq!(sim_state.bit_plane_1, [0x55, 0x55, 0x55, 0x05]);
     }
 
     #[test]
@@ -533,19 +538,19 @@ mod tests {
         let sim_state = SimState {
             order: 0,
             bit_len: 27,
-            state_bits: vec![0xAA, 0xAA, 0xAA, 0x02],
-            valid_bits: vec![0x55, 0x55, 0x55, 0x05],
+            bit_plane_0: vec![0xAA, 0xAA, 0xAA, 0x02],
+            bit_plane_1: vec![0x55, 0x55, 0x55, 0x05],
         };
-        let mut state_bits = [0u8; 2];
-        let mut valid_bits = [0u8; 2];
-        sim_state.get_net(0, nz!(9), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0xAA, 0b0]);
-        assert_eq!(valid_bits, [0x55, 0b1]);
-        sim_state.get_net(9, nz!(9), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0x55, 0b1]);
-        assert_eq!(valid_bits, [0xAA, 0b0]);
-        sim_state.get_net(18, nz!(9), &mut state_bits, &mut valid_bits);
-        assert_eq!(state_bits, [0xAA, 0b0]);
-        assert_eq!(valid_bits, [0x55, 0b1]);
+        let mut bit_plane_0 = [0u8; 2];
+        let mut bit_plane_1 = [0u8; 2];
+        sim_state.get_net(0, nz!(9), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0xAA, 0b0]);
+        assert_eq!(bit_plane_1, [0x55, 0b1]);
+        sim_state.get_net(9, nz!(9), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0x55, 0b1]);
+        assert_eq!(bit_plane_1, [0xAA, 0b0]);
+        sim_state.get_net(18, nz!(9), &mut bit_plane_0, &mut bit_plane_1);
+        assert_eq!(bit_plane_0, [0xAA, 0b0]);
+        assert_eq!(bit_plane_1, [0x55, 0b1]);
     }
 }
